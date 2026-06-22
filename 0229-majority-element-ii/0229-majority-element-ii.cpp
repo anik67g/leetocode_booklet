a55b8d794b9c3b9bb6ce40d72015840927a8eae4
Anikesh mishra <150847193+anik67g@users.noreply.github.com>
2026-06-22 21:45:15 +0530

    Time: 0 ms (100%), Space: 23.5 MB (96.88%) - LeetHub

---
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        int el1= INT_MIN ;
+         int el2= INT_MIN;
+        int cnt1=0 ;
+         int cnt2=0;
+        for(int i=0 ;i<n;i++){
+            if(cnt1==0 && el2!=nums[i]){
+                cnt1 =1;
+                el1= nums[i];
+            }
+            else  if(cnt2==0 && el1!=nums[i]){
+                cnt2 =1;
+                el2= nums[i];
+            }
+            else if (el1==nums[i]){
+                cnt1++;
+            }
+                        else if (el2==nums[i]){
+                cnt2++;
+            }
+            else {
+                cnt1--;
+                cnt2--;
+            }
+
+        }
+        cnt1=0 ;
+         cnt2=0;
+        // check the apperance how many times elemets appear in the array
+        for(int i=0 ;i<n ;i++){
+            if(nums[i]==el1){
+                cnt1++;
+            }
+            if(nums[i]==el2){
+                cnt2++;
+            }
+        }
+        // to store the majority element in the list;
+        vector <int> res;
+        int mini =( (int)n/3)+1;
+      if (cnt1>=mini) res.push_back(el1);
+      if (cnt2>=mini ) res.push_back(el2);
+       return res;
+    }
+
+    
+};
