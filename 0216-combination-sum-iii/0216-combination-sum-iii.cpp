2df5ff68004efeaf8485b9fb99954fa12dd7e7fa
Anikesh mishra <150847193+anik67g@users.noreply.github.com>
2026-06-24 22:08:56 +0530

    Time: 2 ms (20.87%), Space: 8.9 MB (32.07%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,39 +1,32 @@
 class Solution {
-private: 
-    // Recursive function to find combinations
-    void func(int sum, int last, vector<int> &nums, int k, vector<vector<int>> &ans) {
-        // If the sum is zero and the number of elements is k
-        if(sum == 0 && nums.size() == k) {
-            // Add the current combination to the answer
-            ans.push_back(nums);
-            return; 
+private:
+    void solve(int i, vector<int>& temp, vector<vector<int>>& ans, int k,
+          int target) {
+        if (target < 0 || temp.size() > k) {
+            return;
         }
-        // If the sum is less than or equal to zero or the number of elements exceeds k
-        if(sum <= 0 || nums.size() > k) return; 
+        if (target == 0 && temp.size() == k) {
+            ans.push_back(temp);
+            return;
+        }
 
-        // Iterate from the last number to 9
-        for(int i = last; i <= 9; i++) {
-            // If the current number is less than or equal to the sum
-            if(i <= sum) {
-                // Add the number to the current combination
-                nums.push_back(i); 
-                // Recursive call with updated sum and next number
-                func(sum - i, i + 1, nums, k, ans); 
-                // Remove the last number to backtrack
-                nums.pop_back(); 
-            } else {
-                // If the number is greater than the sum, break the loop
-                break;
-            }
-        }
-    } 
+    if(i<10){
+        temp.push_back(i);
+        solve(i + 1, temp, ans, k, target - i);
+        temp.pop_back();
+        solve(i + 1, temp, ans, k, target);}
+    }
+
 public:
-    // Function to find all possible combinations of k numbers that add up to n
     vector<vector<int>> combinationSum3(int k, int n) {
-        vector<vector<int>> ans; 
-        vector<int> nums; 
-        // Call the recursive function with initial parameters
-        func(n, 1, nums, k, ans);
-        return ans; 
+        vector<vector<int>> ans;
+        vector<int> temp;
+
+        if (k > n)
+            return ans;
+
+        solve(1, temp, ans, k, n);
+
+        return ans;
     }
 };
