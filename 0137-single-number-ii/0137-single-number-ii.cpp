a3793570480041add76cc947fb60ce7079a96a08
Anikesh mishra <150847193+anik67g@users.noreply.github.com>
2026-07-18 00:06:53 +0530

    Time: 1 ms (44.08%), Space: 13.4 MB (34.24%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-       int n = nums.size();
-       int ans ;
-       unordered_map<int,int> mpp;
-       for(int i = 0;i<n;i++){
-        mpp[nums[i]]++;
-       } 
-       for(auto it:mpp){
-        if(it.second==1){
-            ans = it.first;
+        int n= nums.size();
+        sort(nums.begin() ,nums.end());
+        for(int i = 1 ;i<n;i+=3){
+            if(nums[i]!=nums[i-1]){
+                return nums[i-1];
+            }
         }
-       }
-return ans;
+        return nums[n-1];
     }
 };
