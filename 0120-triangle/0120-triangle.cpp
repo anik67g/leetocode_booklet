abad68825e367f3c39327c1328d4437e00c0ae29
Anikesh mishra <150847193+anik67g@users.noreply.github.com>
2026-07-31 00:58:58 +0530

    Time: 0 ms (100%), Space: 13.5 MB (9.12%) - LeetHub

---
+++ 0120-triangle/0120-triangle.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n =  triangle.size();
+        
+        vector<vector<int>> dp = triangle; // copy same shape
+      for(int j = 0 ;j<n;j++){
+        dp[n-1][j]=triangle[n-1][j];
+      }
+        for(int  i = n-2;i>=0;i--){
+            for(int j = i ;j>=0;j--){
+               
+                int down=triangle[i][j]+dp[i+1][j];
+                int diagnol=triangle[i][j]+dp[i+1][j+1];
+                 dp[i][j]= min(down,diagnol);
+            }
+           
+        }
+        return dp[0][0];
+      
+    }
+};
