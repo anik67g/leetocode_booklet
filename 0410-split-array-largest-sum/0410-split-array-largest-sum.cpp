79317b666da8b792a3f64c1d025f1e9209233b32
Anikesh mishra <150847193+anik67g@users.noreply.github.com>
2026-08-25 21:53:07 +0530

    Time: 0 ms (100%), Space: 10.6 MB (10.76%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,39 +1,37 @@
 class Solution {
-private:
-    vector<int> prefixSum;
-    int dp[51][1001];
-
-    int solve(const vector<int>& nums, int k, int idx){
-        int n = nums.size();
-
-        if(idx >= n) return 0;
-
-        if(k == 1) return prefixSum[n] - prefixSum[idx];
-
-        if(dp[k][idx] != -1) return dp[k][idx];
-
-        int ans = INT_MAX;
-
-        for(int i=idx; i<=n-k; i++){
-            int leftPartition = prefixSum[i+1] - prefixSum[idx];
-            int rightPartition = solve(nums, k-1, i+1);
-
-            int mxValueBetweenPartiton = max(leftPartition, rightPartition);
-
-            ans = min(ans, mxValueBetweenPartiton);
-        }
-
-        return dp[k][idx] = ans;
-    }
 public:
+int isallocate(vector<int>&nums , int barrier){
+   int n = nums.size();
+   int stud = 1;
+   long long pages = 0;
+
+   for(int i = 0 ;i<n;i++){
+      if(pages+nums[i] <=barrier){
+        pages+=nums[i];
+      }
+      else{
+        stud++;
+        pages = nums[i];
+      }
+   }
+
+   return stud;
+}
     int splitArray(vector<int>& nums, int k) {
         int n = nums.size();
-
-        prefixSum.assign(n+1, 0);
-        for(int i=0; i<n; i++) prefixSum[i+1] = prefixSum[i] + nums[i];
-
-        memset(dp, -1, sizeof(dp));
+        long long low= *max_element(nums.begin() ,nums.end());
         
-        return solve(nums, k, 0);
+        long long high = accumulate(nums.begin() ,nums.end() , 0LL);
+
+        while(low<=high){
+            long long mid = (low+high)/2;
+            if(isallocate(nums,mid)>k){
+             low =mid+1;
+            }
+            else{
+              high = mid-1;
+            }
+        }
+        return low;
     }
 };
